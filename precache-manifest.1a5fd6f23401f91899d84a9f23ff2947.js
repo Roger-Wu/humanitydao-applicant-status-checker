7f853f2fe0501edfd21c262dca3e410276b9104c
Roger-Wu <gsetcrw@gmail.com>
2019-08-02 09:57:54 +0800

    init

---
+++ precache-manifest.1a5fd6f23401f91899d84a9f23ff2947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "787af5398f15b6536b77",
+    "url": "./static/js/main.787af539.chunk.js"
+  },
+  {
+    "revision": "317a925252a3e4b6f85e",
+    "url": "./static/js/1.317a9252.chunk.js"
+  },
+  {
+    "revision": "787af5398f15b6536b77",
+    "url": "./static/css/main.15e9113d.chunk.css"
+  },
+  {
+    "revision": "42a8939344a17e0fff2190c564a1481c",
+    "url": "./index.html"
+  }
+];
